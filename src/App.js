f5f922656dad1c10ab5d7aa758a13c01c208bd1c
roxannearriola <roxanne.arriola@gmail.com>
2021-07-05 12:32:12 -0400

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Weather from "./Weather";
+import Footer from "./Footer";
+
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
 			<Weather />
+			<Footer />
 		</div>
 	);
 }
---
+++ src/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./Footer.css";
+
+export default function Footer() {
+	return (
+		<div className="Footer">
+			<a
+				href="https://github.com/roxannearriola/weather-app-react"
+				target="_blank"
+			>
+				Open-source code
+			</a>{" "}
+			by Roxanne A.
+			<br />
+			Icons made by{" "}
+			<a href="https://www.freepik.com" title="Freepik">
+				{" "}
+				Freepik
+			</a>{" "}
+			from{" "}
+			<a href="https://www.flaticon.com/" title="Flaticon">
+				www.flaticon.com
+			</a>
+		</div>
+	);
+}
